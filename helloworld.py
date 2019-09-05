f3c66b6161b6f1f4494836e045d26736004ee65c
vishal sobani <vishal.sobani@gmail.com>
2019-09-05 08:06:30 +0530

    Update changes

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,22 @@
-print("'please don't' be late for class")
+for i in range(1,11):
+    print (i)
+
+
+a='vishal'
+b=30
+
+print(a + str(b))
+print(a,str(b))
+print("vis","hal", "hello", sep="\n")
+print("","home","vishal", "tarzan","Hello world",sep="/")
+
+
+print("root","node","child",sep='->')
+print("vish","al",end='...')
+print('\n')
+print('one','two','three\n','four','five','six\n''seven','eight','nine')
+
+print('one','two','three',end='')
+print('four''five''six',end='')
+print('seven','eight','nine',end='sep')
+
